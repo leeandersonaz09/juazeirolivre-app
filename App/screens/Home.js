1766ba0982bb5d123f25921b728c92714385ba62
Márcio Campos <msfcampos05@gmail.com>
2020-07-02 16:58:24 -0300

    Update Home.js

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -8,34 +8,54 @@ import {
     SafeAreaView,
     ScrollView,
     Share,
+    TextInput,
     ActivityIndicator,
-    RefreshControl
+    RefreshControl,
+    Dimensions
 } from 'react-native';
 //import Card from '../components/Card'
 import { Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Container } from 'native-base';
 import Header from '../components/Header';
 import * as firebase from 'firebase';
 import { FlatList } from 'react-native-gesture-handler';
-
+const { height, width } = Dimensions.get('window')
 import LoadingComponent from '../components/defaultLoading/lottieLoading';
-import Loading from '../loaders/spinner.json';
+import Loading from '../loaders/13255-loader.json';
 
 const Home = ({ navigation }) => {
 
     const dataRef = firebase.firestore().collection('post');
     let onEndReachedCalledDuringMomentum = false;
     const [data, setData] = useState([]);
+    const [query, setQuery] = useState(null);
+    const [barIcon, setbarIcon] = useState('https://img.icons8.com/ios/100/000000/search--v1.png');
     const [dataBackup, setdataBackup] = useState([]);
-    const [loading, setLoading] = useState(true); // Set loading to true on component mount
-
+    const [loading, setLoading] = useState(true);
+    const [pageSize, setpageSize] = useState(5);
 
     useEffect(() => {
-        if (loading) LoadingAnimation();
-        const subscriber = dataRef.orderBy('data', 'desc').limit(10)
+
+        const subscriber = getData();
+        // Unsubscribe from events when no longer in use
+        return () => subscriber;
+
+
+    }, []);
+
+    //Old Load componet
+    /*
+    if (!loading) {
+         return <View style={styles.loading}><ActivityIndicator size='large' /></View>
+    }
+    */
+    const getData = async () => {
+        setLoading(true);
+        await dataRef.orderBy('data', 'desc').limit(pageSize)
             .onSnapshot(querySnapshot => {
 
                 const list = [];
                 querySnapshot.forEach(doc => {
+                    console.log(list)
                     const { by, data, img, ref, text, tittle, avatar } = doc.data();
                     list.push({
                         id: doc.id,
@@ -48,26 +68,58 @@ const Home = ({ navigation }) => {
                         avatar
                     });
                 });
-
+                console.log('data obj' + list)
                 setdataBackup(list);
                 setData(list);
-                setLoading(false);
+                setTimeout(() => {
+                    setLoading(false);
+                }, 2000);
             });
-        // Unsubscribe from events when no longer in use
-        return ()=> subscriber;
 
 
-    }, []);
 
-    //Old Load componet
-    /*
-    if (!loading) {
-         return <View style={styles.loading}><ActivityIndicator size='large' /></View>
     }
-    */
+
+    const loadMore = () => {
+        var items = pageSize + 5;
+        setpageSize(items);
+    }
 
     const LoadingAnimation = () => {
         return <LoadingComponent data={Loading} />;
+    }
+    //SearchBar working 
+    const filterItem = (value) => {
+
+        //Armazena texto do input search
+        var text = value;
+
+        if (text == '') {
+            setbarIcon('https://img.icons8.com/ios/100/000000/search--v1.png');
+        } else {
+            setbarIcon('https://img.icons8.com/ios/50/000000/left.png');
+        }
+
+        setQuery(text)
+
+        const newData = dataBackup.filter(item => {
+            const itemData = `${item.data.toUpperCase()} ${item.tittle.toUpperCase()} ${item.text.toUpperCase()}`;
+            const textData = text.toUpperCase();
+
+            return itemData.indexOf(textData) > -1;
+        });
+
+        setData(newData);
+    };
+
+    const searchIconBack = () => {
+
+        if (barIcon == 'https://img.icons8.com/ios/50/000000/left.png') {
+            setbarIcon('https://img.icons8.com/ios/100/000000/search--v1.png')
+            setQuery(null)
+            getData();
+        }
+
     }
 
     const shareContent = async () => {
@@ -92,18 +144,18 @@ const Home = ({ navigation }) => {
     const renderList = ({ by, data, img, ref, text, tittle, id, avatar }) => {
         return (
             <>
-                <TouchableOpacity onPress={() => {
-                        navigation.navigate('ProductDetails', {
-                            id: id,
-                            tittle: tittle,
-                            ref: ref,
-                            img: img,
-                            text: text,
-                            data: data,
-                            avatar: avatar,
-                            by: by
-                        })
-                    }}>
+                <TouchableOpacity activeOpacity={0.8} onPress={() => {
+                    navigation.navigate('ProductDetails', {
+                        id: id,
+                        tittle: tittle,
+                        ref: ref,
+                        img: img,
+                        text: text,
+                        data: data,
+                        avatar: avatar,
+                        by: by
+                    })
+                }}>
                     <Card style={{ flex: 0 }}>
                         <CardItem>
                             <Left>
@@ -118,11 +170,12 @@ const Home = ({ navigation }) => {
                         <CardItem>
                             <Body>
                                 <Text style={{ fontWeight: 'bold', marginBottom: 10 }}>{tittle}</Text>
-                                <Image resizeMode={'cover'} source={{ uri: img }} style={{ height: 200, width: '100%' }} />
+                                <Image source={{ uri: img }} style={{ flex: 1, height: 300, width: '100%', resizeMode: 'contain' }} />
                                 <Text style={styles.Text}>{text}</Text>
                                 <Text style={{ fontStyle: 'italic', color: "#808080", textAlign: 'center', marginTop: 10 }}>
                                     {ref}
                                 </Text>
+
                             </Body>
                         </CardItem>
 
@@ -148,18 +201,45 @@ const Home = ({ navigation }) => {
                 <Text style={styles.headerTitle}>Juazeiro Livre</Text>
             </Header>
             <Content>
+                <View style={styles.header}>
+                    <View style={styles.SectionStyle}>
+                        <TouchableOpacity onPress={() => searchIconBack()}>
+                            <Image
+                                //We are showing the Image from online
+                                source={{ uri: barIcon }}
+                                //Image Style
+                                style={styles.ImageStyle}
+                            />
+                        </TouchableOpacity>
+                        <TextInput
+                            underlineColorAndroid="transparent"
+                            placeholder="O que procura..."
+                            placeholderTextColor="gray"
+                            value={query}
+                            onChange={(event) => {
+                                setQuery(event.value);
+                                filterItem(event.target.value);
+                            }}
+                            style={styles.input}
+                        />
+                    </View>
+                </View>
                 <Image
                     style={styles.headerImage}
                     source={require('../../assets/home-img.png')}
                 />
                 <View style={styles.contentContainer}><Text style={styles.Tittle}>Postagens Recentes</Text>
                     <View style={styles.cardContainer}>
-
-                        <FlatList
-                            data={data}
-                            keyExtractor={item => item.id.toString()}
-                            renderItem={({ item }) => loading ? LoadingAnimation() : renderList(item)}
-                        />
+                        {loading ? LoadingAnimation() :
+                            (<FlatList
+                                data={data}
+                                keyExtractor={item => String(item.id)}
+                                renderItem={({ item }) => data ? renderList(item) : <Text style={{ alignSelf: 'center', justifyContent: 'center' }}>Não há novas postagens</Text>}
+                                onEndReached={() => loadMore()}
+                                onEndReachedThreshold={0.1}
+                                ListFooterComponent={loading && <LoadingComponent data={Loading} />}
+                            />)
+                        }
                     </View>
                 </View>
             </Content>
@@ -183,6 +263,42 @@ const styles = StyleSheet.create({
         color: "#fff",
         fontWeight: "bold",
     },
+
+    header: {
+        height: 60,
+        width: '100%',
+        backgroundColor: '#3f51b5',
+        justifyContent: 'center',
+        alignContent: 'center',
+        alignItems: 'center',
+      },
+      input: {
+        height: 40,
+        width: '90%',
+        backgroundColor: '#fff',
+        borderRadius: 20,
+        padding: 5,
+        paddingLeft: 10,
+      },
+      SectionStyle: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        height: 40,
+        borderRadius: 20,
+        margin: 10,
+      },
+    
+      ImageStyle: {
+        padding: 10,
+        margin: 5,
+        height: 25,
+        width: 25,
+        resizeMode: 'stretch',
+        alignItems: 'center',
+      },
+
     loading: {
         position: 'absolute',
         left: 0,
@@ -192,6 +308,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
     },
+
     headerImage: {
         width: "100%",
         height: 300,
