097d4a39026966e8490cc81c69dfae392c03cd48
Lee Brasil <leeandersonaz09@gmail.com>
2020-07-11 23:48:13 -0300

    update

--- App/screens/Welcome/Content.tsx
+++ App/screens/Welcome/Content.tsx
@@ -1,22 +1,8 @@
 import React from "react";
-import { Dimensions, StyleSheet} from "react-native";
+import { Dimensions } from "react-native";
 import { Image, Rect, TSpan, Text, } from "react-native-svg";
-const { width, height } = Dimensions.get("window");
 
-const styles = StyleSheet.create({
-  title1: {
-    fontSize: 48,
-    fontWeight: "300",
-  },
-  title2: {
-    fontSize: 48,
-    fontWeight: "600",
-  },
-  description: {
-    opacity: 0.5,
-    fontSize: 16,
-  },
-});
+const { width, height } = Dimensions.get("window");
 
 interface ContentProps {
   color: string;
@@ -42,8 +28,8 @@ export default ({
 }: ContentProps) => {
   return (
     <>
-      <Rect x={0} y={0} {...{ width, height }} fill={backgroundColor} clipPath="url(#mask)"/>
-      <Image x={16} y={100} width={width * 0.61} href={source} clipPath="url(#mask)"/>
+      <Rect x={0} y={0} {...{ width, height }} fill={backgroundColor} clipPath="url(#mask)" />
+      <Image x={16} y={100} width={width * 0.61} href={source} clipPath="url(#mask)" />
       <Text x={16} y={height / 2} fontSize={48} fontWeight={300} fill={color} clipPath="url(#mask)">
         {title1}
       </Text>
@@ -60,14 +46,14 @@ export default ({
       <Text y={height / 2 + 48 * 2} fontSize={16} fill={color} clipPath="url(#mask)">
         <TSpan x={16} dy={0}>
           Lorem ipsum dolor sit amet,
-    </TSpan>
+        </TSpan>
         <TSpan x={16} dy={16 + 2}>
           Lorem ipsum dolor sit amet,
-    </TSpan>
+        </TSpan>
         <TSpan x={16} dy={32 + 4}>
           Lorem ipsum dolor sit amet,
-    </TSpan>
-      </Text>
+        </TSpan>
+    </Text>
 
     </>
   );
