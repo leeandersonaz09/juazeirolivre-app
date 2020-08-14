6a19d84d42ee217bf2eee9c37c46e1c1257470ed
Lee Brasil <lee.brasil@outlook.com>
2020-08-14 17:02:08 -0300

    update

--- src/components/View/index.tsx
+++ src/components/View/index.tsx
@@ -5,7 +5,7 @@ import { useTheme } from '@react-navigation/native';
 export default function View(props) {
   const { colors } = useTheme();
   const theme = useTheme();
-  const color = theme ? colors.background : "#444";
+  const color = theme ? "#444" : colors.background;
 
   return (
     <>
